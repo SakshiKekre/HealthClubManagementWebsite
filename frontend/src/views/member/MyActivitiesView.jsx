5aa7e61e730b37da74f548c4d7bee26653dd3496
nagabathula <naga.bathula@gmail.com>
2023-05-12 00:49:12 -0700

    Adding env file

--- frontend/src/views/member/MyActivitiesView.jsx
+++ frontend/src/views/member/MyActivitiesView.jsx
@@ -1,30 +1,29 @@
-import React, { lazy, Component } from "react";
-const ActivityList = lazy(() => import("../../components/member/ActivityList"));
+import React, { lazy } from "react";
+import { useSelector } from "react-redux";
+import ActivityList from "../../components/member/ActivityList";
+import LeftNavigation from "../../components/LeftNavigation";
 
+const MyActivitiesView = () => {
+  // const userName = useSelector((state) => state.authReducer.userName);
+  // const userRole = useSelector((state) => state.authReducer.userRole);
 
-class MyActivitiesView extends Component {
-  state = { imagePreview: "", isDeleting: false };
-
-  onSubmit = async (values) => {
+  const onSubmit = async (values) => {
     alert(JSON.stringify(values));
   };
 
-
-  render() {
-    return (
-      <div className="container-fluid my-3">
-         <div className="row">
-          <div className="col-md-20"> 
+  return (
+    <div className="container-fluid my-3">
+      <div className="row">
+        <div className="col-md-2">
+          <LeftNavigation />
+        </div>
+        <div className="col-md-10">
           <h4 className="text-center">View Member Activities</h4>
-            <ActivityList
-              onSubmit={this.onSubmit}
-            />
-          </div>
-
+          <ActivityList onSubmit={onSubmit} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MyActivitiesView;
