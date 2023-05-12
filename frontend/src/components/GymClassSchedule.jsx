3b3f0bed0b834b23224ac010c8c902eb7ceecfe1
Sakshi Kekre <sakshi.kekre@gmail.com>
2023-05-12 04:22:21 -0700

    Class Schedule and location for home page

--- frontend/src/components/GymClassSchedule.jsx
+++ frontend/src/components/GymClassSchedule.jsx
@@ -1,67 +1,34 @@
 import { useState, useEffect } from 'react';
 import { Dropdown } from 'react-bootstrap';
 
-// Define constant data for locations and class schedules
-const LOCATIONS = ['San Jose', 'Fremont', 'San Francisco', 'Dublin'];
-const SCHEDULES = {
-  'San Jose': [
-    { id: 1, className: 'Yoga', time: '9:00am - 10:00am' },
-    { id: 2, className: 'Pilates', time: '10:00am - 11:00am' },
-    { id: 3, className: 'Zumba', time: '11:00am - 12:00pm' },
-  ],
-  'Fremont': [
-    { id: 4, className: 'Yoga', time: '10:00am - 11:00am' },
-    { id: 5, className: 'Pilates', time: '11:00am - 12:00pm' },
-    { id: 6, className: 'Spinning', time: '12:00pm - 1:00pm' },
-  ],
-  'San Francisco': [
-    { id: 7, className: 'Yoga', time: '11:00am - 12:00pm' },
-    { id: 8, className: 'Zumba', time: '12:00pm - 1:00pm' },
-    { id: 9, className: 'Spinning', time: '1:00pm - 2:00pm' },
-  ],'Dublin': [
-    { id: 10, className: 'Yoga', time: '11:00am - 12:00pm' },
-    { id: 11, className: 'Zumba', time: '12:00pm - 1:00pm' },
-    { id: 12, className: 'Spinning', time: '1:00pm - 2:00pm' },
-  ],
-};
-
 function GymClassSchedule() {
-  const [locations, setLocations] = useState(LOCATIONS);
-  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [locations, setLocations] = useState([]);
   const [schedule, setSchedule] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [selectedLocationId, setSelectedLocatioId] = useState("");
+
+  const handleLocationDropdownChange = (locationName,locationID) => {
+    console.log('inside handleLocationDropdownChange');
+    console.log('locationName'+locationName);
+    console.log('locationID'+locationID);
+
+    setSelectedLocation(locationName);
+    setSelectedLocatioId(locationID);
+  }
 
   useEffect(() => {
     // Fetch the list of locations and set the state
-    fetch('api/locations')
+    fetch(process.env.REACT_APP_API_URL +"/findAllLocations")
       .then(response => response.json())
       .then(data => setLocations(data))
       .catch(error => console.error(error));
   }, []);
 
-  // useEffect(() => {
-  //   if (selectedLocation) {
-  //     // Fetch the schedule for the selected location and set the state
-  //     fetch(`api/schedule/${selectedLocation}`)
-  //       .then(response => response.json())
-  //       .then(data => setSchedule(data))
-  //       .catch(error => console.error(error));
-  //   }
-  // }, [selectedLocation]);
-
-  useEffect(() => {
-    if (selectedLocation) {
-      // Set the schedule for the selected location from the constant data
-      setSchedule(SCHEDULES[selectedLocation]);
-    }
-  }, [selectedLocation]);
-
-
-  function handleLocationChange(event) {
-    const location = event.target.value;
-    setSelectedLocation(location);
-  }
-  
-
+  useEffect(() =>{
+    fetch(process.env.REACT_APP_API_URL +"/getClassesByLoc/"+selectedLocationId)
+    .then((response) => response.json())
+    .then(data => setSchedule(data))
+  },[selectedLocationId]);
 
   return (
     <>
@@ -72,22 +39,33 @@ function GymClassSchedule() {
         </Dropdown.Toggle>
         <Dropdown.Menu>
           {locations.map(location => (
-            <Dropdown.Item key={location} value={location} onClick={handleLocationChange}>
-              {location}
+            <Dropdown.Item key={location.locationID} value={location.locationID} onClick={() => handleLocationDropdownChange(location.locationName, location.locationID)}>
+              {location.locationName}
             </Dropdown.Item>
           ))}
         </Dropdown.Menu>
       </Dropdown>
       <div className="row mt-4">
         <div className="col-md-6">
-          <h4>Class Schedule - {selectedLocation}</h4>
-          <ul>
-            {schedule.map(classSchedule => (
-              <li key={classSchedule.id}>
-                {classSchedule.className} - {classSchedule.time}
-              </li>
-            ))}
-          </ul>
+          <div class="container">
+
+            <h4>Class Schedule - {selectedLocation}</h4>
+            <ul className="list-unstyled">
+             {schedule.length > 0 ? (
+                schedule.map((classSchedule) => (
+                  <div className="card membership-block"> 
+                    <li key={classSchedule.id}>
+                      {classSchedule.className} - {classSchedule.time}
+                    </li>
+                  </div>
+                ))
+              ) : (
+                <div className="card membership-block">
+                  <li>No class schedule available</li>
+                </div>
+              )}
+            </ul>
+          </div> 
         </div>
         <div className="col-md-6">
           <h4>Class Instructors</h4>
