46cd86816c7c7f145b1304cce828ef4e5b27d679
nagabathula <naga.bathula@gmail.com>
2023-05-12 11:20:03 -0700

    Fixed Success Message

--- frontend/src/views/account/ForgotPassword.jsx
+++ frontend/src/views/account/ForgotPassword.jsx
@@ -3,22 +3,25 @@ const ForgotPasswordForm = lazy(() =>
   import("../../components/account/ForgotPasswordForm")
 );
 
-class ForgotPasswordView extends Component {
-  onSubmit = async (values) => {
-    alert(JSON.stringify(values));
+function ForgotPasswordView()  {
+
+  const [errorMessage, setErrorMessage] = React.useState('');
+
+  const onSubmit = async (values) => {
+    setErrorMessage('Email sent to '+values.email +' sucessfully');
   };
-  render() {
+
     return (
       <div className="container my-3">
         <div className="row justify-content-md-center ">
           <div className="col-md-4 p-3 border">
             <h4 className="text-center">Forgot Password</h4>
-            <ForgotPasswordForm onSubmit={this.onSubmit} />
+            <ForgotPasswordForm onSubmit={onSubmit} />
+            <div className="text-center text-danger font-weight-bold">{errorMessage}</div>
           </div>
         </div>
       </div>
     );
   }
-}
 
 export default ForgotPasswordView;
